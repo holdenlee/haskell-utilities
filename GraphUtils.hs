c8ea04e4e20cce4924d047459d5f014c6d50cf04
Holden Lee <holdenl@princeton.edu>
2015-11-19 22:14:47 -0500

    add graph, set utilities

--- GraphUtils.hs
+++ GraphUtils.hs
@@ -1,3 +1,8 @@
+{-# OPTIONS
+
+  -XRank2Types
+#-}
+
 module GraphUtils where
 import System.Environment
 import Control.Monad
@@ -10,14 +15,33 @@ import qualified Data.Set as S
 import Data.Array
 import Data.Tuple
 import Data.Graph.Inductive as G
-import Search
+import Control.Lens hiding ((|>))
+--import Search
 
 import Utilities
-import ParseUtilities
 
+newNode :: G.Gr a b -> Node
+newNode g = (newNodes 1 g)!!0
+
+contextToEdges :: G.Context a b -> [LEdge b]
+contextToEdges (ins, v, _, outs) =
+    (L.map (\(n,x) -> (x, v, n)) ins) ++
+    (L.map (\(n,y) -> (v, y, n)) outs)
+
+insContext = (G.&)
+
+{-
+insContext :: G.Context a b -> G.Gr a b -> G.Gr a b
+insContext (ins, v, x, outs) g = g |> insNode (v,x)
+                                   |> insEdges (contextToEdges (ins, v, x, outs))-}
 
 getEdges:: G.Gr a b -> Node -> Node -> [b]
 getEdges g s t = map snd $ filter (\(v,l) -> v==t) $ lsuc g s
+
+modV :: Node -> (a -> a) -> G.Gr a b -> G.Gr a b
+modV v f g = let (ins, _, n, out) = context g v in
+             g |> delNode v
+               |> insContext (ins, v, f n, out)
 
 modEdge:: (Eq b) => Edge -> (b-> b) -> G.Gr a b -> G.Gr a b
 modEdge (v,w) f g = g |> delEdge (v,w) |> (insEdges $ map (\elab -> (v,w,f elab)) (getEdges g v w))
@@ -33,3 +57,23 @@ mkBidir def vs es =
         es2 = L.unionBy (\(x,y,_) (x1,y1,_) -> x==x1 && y==y1) es es'
     in 
       G.mkGraph vs es2
+
+contextLens :: G.Node -> Lens' (G.Gr a b) (G.Context a b)
+contextLens n = lens (flip G.context $ n) 
+                     (\g ctxt -> 
+                          g |> delNode n 
+                            |> insContext ctxt)
+{-(ins, m, lab, outs)
+                            |> insNode (n, lab)
+                            |> insEdges (L.map (\(l, x) -> (x, n, l)) ins)
+                            |> insEdges (L.map (\(l, x) -> (n, x, l)) outs))
+-}
+
+nodeLens :: G.Node -> Lens' (G.Gr a b) a
+nodeLens n = (contextLens n) . _3
+
+insLens :: G.Node -> Lens' (G.Gr a b) (G.Adj b)--[(b, Node)]
+insLens n = (contextLens n) . _1
+
+outsLens :: G.Node -> Lens' (G.Gr a b) (G.Adj b) --[(b, Node)]
+outsLens n = (contextLens n) . _4
--- Utilities.hs
+++ Utilities.hs
@@ -8,20 +8,45 @@
  -XPolyKinds
 #-}
 
-module Utilities  where
+module Utilities where
 import System.Environment
 import Control.Monad
 import Data.Tree
-import qualified Data.List
+import Data.List
 import qualified Data.Map.Strict as Map
 import qualified Data.Hashable
+import Data.Either
 import Data.Maybe
 import Debug.Trace
 
---combinators
+-- * Function combinators
+
+infixr 0 `c2`
+c2 :: (c -> d) -> (a -> b -> c) -> (a -> b -> d)
+c2 f g x y = f $ g x y
+--or: c2 f g = (f .) . (g .)
+--or (point-free): (.).(.)
+
+infixr 0 `c3`
+c3 :: (d -> e) -> (a -> b -> c -> d) -> (a -> b -> c -> e)
+c3 f g x y z = f $ g x y z
+--or: c3 f g = ((f .) .) . ((g .) .)
+--or (point-free): (.).(.).(.)
 
 doIf :: Bool -> (a -> a) -> (a -> a)
 doIf p f = (\x -> if p then f x else x)
+
+doIfElse :: Bool -> (a -> a) -> (a -> a) -> (a -> a)
+doIfElse p f g = (\x -> if p then f x else g x)
+
+doIfElseCond :: (a -> Bool) -> (a -> a) -> (a -> a) -> a -> a
+doIfElseCond p f g x = if p x then f x else g x
+
+tryWithDefault::(a->Maybe b) -> b -> a -> b
+tryWithDefault f def x = 
+  case (f x) of
+    Nothing -> def
+    Just y -> y
 
 ifelselist:: [(Bool, a)] -> a -> a
 ifelselist li y = 
@@ -34,11 +59,7 @@ iflist li =
   case li of 
     ((b,x):li2) -> if b then x else iflist li2
 
-loopUntilFail :: (a -> Maybe a) -> a -> a
-loopUntilFail f x = 
-  case (f x) of 
-    Just y -> loopUntilFail f y
-    Nothing -> x
+-- * Loops/folds
 
 while :: (a -> Bool) -> (a -> a) -> a -> a
 while p f x = 
@@ -48,33 +69,64 @@ loopUntil :: (a -> Bool) -> (a -> a) -> a -> a
 loopUntil p f x = 
   if (p x) then x else loopUntil p f (f x)
 
+loopUntilFail :: (a -> Maybe a) -> a -> a
+loopUntilFail f x = 
+  case (f x) of 
+    Just y -> loopUntilFail f y
+    Nothing -> x
+
 stopBefore :: (a -> Bool) -> (a -> a) -> a -> a    
 stopBefore p f x = 
   if (p (f x)) then x else stopBefore p f (f x)
 
---unsafe operations
-removeJust:: Maybe a -> a
-removeJust x = case x of Just y -> y
+repeatTimes :: Int -> (a-> a) -> a -> a
+repeatTimes n f x = 
+    if n <=0 then x else repeatTimes (n-1) f (f x)
+--repeatTimes n f = (foldl (.) id $ replicate n f)
 
-removeJustWithDefault :: Maybe a -> a-> a
-removeJustWithDefault x y = case x of
-                      Nothing -> y
-                      Just z -> z
+foldIterate:: (a -> c -> c) -> [a] -> c -> c
+foldIterate f as x = foldl (flip f) x as
 
-justRight :: Either a b -> b
-justRight (Right x) = x
+foldIterate2:: (a -> b -> c -> c) -> [a] -> [b] -> c -> c
+foldIterate2 f as bs x = foldl (\y -> \(xa, xb) -> f xa xb y) x (zip as bs)
 
-lookup2 :: (Ord a) => a -> Map.Map a b -> b
-lookup2 x h = removeJust (Map.lookup x h)
+for :: [a] -> b -> (a -> b -> b) -> b
+for li x0 f = foldl (flip f) x0 li
 
-lookupWithDefault:: (Ord a) => a -> Map.Map a b -> b -> b
-lookupWithDefault x h y = removeJustWithDefault (Map.lookup x h) y
-  
---Maps and Lists
+-- * Function composition
+
+infixr 0 <|
+(<|) :: (a -> b) -> a -> b
+(<|) = ($)
+
+infixl 0 |>
+(|>) :: a -> (a -> b) -> b
+x |> f = f x
+
+infixr 0 <<|
+(<<|) :: (b -> c) -> (a -> b) -> (a -> c)
+(<<|) = (.)
+
+infixl 0 |>>
+(|>>) :: (a -> b) -> (b -> c) -> (a -> c)
+(|>>) = flip (.)
+
+-- * Tuples 
+
+third :: (a,b,c) -> c
+third (_,_,z) = z
+--or: ^. _3
+
+appendFun :: (a -> b) -> a -> (a,b)
+appendFun f x = (x, f x)
+ 
+-- * Maps and Lists
+
+isInitialSegment :: Eq a => [a] -> [a] -> Bool
+isInitialSegment = isJust `c2` stripPrefix
 
 insertMultiple :: (Ord a) => [(a, b)] -> Map.Map a b -> Map.Map a b 
 insertMultiple li h = foldl (\hm -> (\(x,y) -> Map.insert x y hm)) h li
-
 
 listUpdateFun :: Int -> (a-> a) -> [a] -> [a]
 listUpdateFun n f li = listUpdate n (f (li !! n)) li
@@ -93,9 +145,6 @@ replaceSublist m n li li2 =
 listUpdate :: Int -> a -> [a] -> [a]
 listUpdate n x li = replaceSublist n (n+1) [x] li
 
-filterJust:: [Maybe a] -> [a]
-filterJust li = map removeJust (filter isJust li)
-
 filterZip:: (b->Bool) -> [a] -> [b] -> [(a,b)]
 filterZip p as bs = filter (\(x,y) -> p y) (zip as bs)
 
@@ -105,41 +154,17 @@ cofilter p as bs = unzip (filterZip p as bs)
 lookupList:: (Ord a) => [a] -> Map.Map a b -> [Maybe b]
 lookupList as mp = fmap (flip Map.lookup mp) as
 
+--unsafe
 lookupList2:: (Ord a) => [a] -> Map.Map a b -> [b]
-lookupList2 as mp = fmap (flip lookup2 mp) as
+lookupList2 as mp = fmap ((Map.!) mp) as
 
-third :: (a,b,c) -> c
-third (_,_,z) = z
-
-mapThr :: (c->d) -> (a,b,c) -> (a,b,d)
-mapThr f (x,y,z) = (x,y,f z)
-
-inj2:: a -> b -> (a,b)
-inj2 x y = (x,y)
-
-
-inj1:: b -> a -> (a,b)
-inj1 y x = (x,y)
-
+--I'm surprised this doesn't exist.
 mlookup :: Int -> [a] -> Maybe a
-mlookup n li = if 0<=n && n<(length li) then Just (li!!n) else Nothing 
+mlookup n li = if 0<=n && n<(length li) then Just (li!!n) else Nothing
 
-(*>)::(a->b)->(b->c)->(a->c)
-f *> g = g.f
-
-tryWithDefault::(a->Maybe b) -> b-> a -> b
-tryWithDefault f def x = 
-  case (f x) of
-    Nothing -> def
-    Just y -> y
-
-foldIterate:: (a->c->c)->[a]->c->c
-foldIterate f as x = foldl (flip f) x as
-
-foldIterate2:: (a->b->c->c)->[a]->[b]->c->c
-foldIterate2 f as bs x = foldl (\y -> \(xa, xb) -> f xa xb y) x (zip as bs)
-
---for :: a -> (a -> a) -> (a -> Bool) -> (a -> a) -> a
+mindex :: [a] -> Int -> Maybe a
+mindex = flip mlookup
+--or use indexed lens 
 
 --NOTE: if you're doing this a lot, use array instead
 sublist :: Int -> Int -> [a] -> [a]
@@ -148,17 +173,6 @@ sublist m n ls =  take (n-m) . drop m $ ls
 
 map2 :: (a -> b -> c) -> [a] -> [b] -> [c]
 map2 = zipWith
-
-infixr 0 <|
-(<|) :: (a -> b) -> a -> b
-(<|) = ($)
-
-infixl 0 |>
-(|>) :: a -> (a -> b) -> b
-x |> f = f x
-
---debug x y = x
-debug = flip trace
 
 enumerate :: [a] -> [(Int, a)]
 enumerate li = zip [1..] li
@@ -174,3 +188,38 @@ zemap f li = map f (zenumerate li)
 
 keepi :: (Int -> Bool) -> [a] -> [a]
 keepi f li = map snd (filter (f.fst) (enumerate li))
+
+-- * Other
+
+--unsafe
+fromLeft :: Either a b -> Maybe a
+fromLeft x = case x of
+              Left y -> Just y
+              Right _ -> Nothing
+
+fromRight :: Either a b -> b
+fromRight (Right x) = x
+
+-- * Debugging
+
+-- debug x y = x
+
+debug = flip trace
+
+debugShow x = trace (show x) x
+
+debugSummary x f = trace (show (f x)) x
+
+-- * Numbers
+
+lclamp :: (Ord a) => a -> a -> a
+lclamp lo x = if x < lo then lo else x
+
+rclamp :: (Ord a) => a -> a -> a
+rclamp hi x = if x > hi then hi else x
+
+clamp :: (Ord a) => a -> a -> a -> a
+clamp lo hi x 
+    | x > hi    = hi
+    | x < lo    = lo
+    | otherwise = x
